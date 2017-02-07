7ef3665bec0794babc0af65fcff027b5a8eeb173
Keith Gordon <keithg07@gmail.com>
2017-02-07 17:24:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-kittens = ["Milo","Otis","Garfield"];
+var kittens = ["Milo","Otis","Garfield"];
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
@@ -17,5 +17,17 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
- return [...kittens, name]
+ return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
 }
