b3b48899ee322cb7b9e1373e6424d96ca43f8e2a
Alexander Sorochinsky <alexander.sorochinskiy@gmail.com>
2017-02-15 11:55:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 const app = "I don't do much."
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+];
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+destructivelyAppendKitten("Ralph");
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+destructivelyPrependKitten("Bob");
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+destructivelyRemoveLastKitten();
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+destructivelyRemoveFirstKitten();
+
+function appendKitten(name) {
+  var newKittens = kittens.slice(0);
+  newKittens.push(name);
+  return newKittens;
+}
+appendKitten("Kek");
+
+function prependKitten(name) {
+  var newKittens = kittens.slice(0);
+  newKittens.unshift(name);
+  return newKittens;
+}
+prependKitten("Monk");
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0);
+  newKittens.pop();
+  return newKittens;
+}
+removeLastKitten();
+
+function removeLastKitten() {
+  var newKittens - kittens.slice(0);
+  newKittens.shift();
+  return newKittens;
+}
+removeLastKitten();
