742f35ca3c89be6fcffbf8099995ddac22addf57
taylor watson <taylorwatson18@icloud.com>
2017-04-14 21:14:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  var newarray = [...kittens]
+  newarray.pop()
+  return newarray
+}
+
+function removeFirstKitten() {
+  var newarray = [...kittens]
+  newarray.shift()
+  return newarray
+}
