eeba2b1c30e809432f83af7f71b0c43cd5a1a87b
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-04-26 15:23:22 +0530

    Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return Array.prototype.concat(kittens, name);
+}
+
+function prependKitten(name) {
+  return Array.prototype.concat(name, kittens);
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
