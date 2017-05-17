27f71be892201e503cc0281a6cfd06cdd48f7c4e
Artem Arbatskiy <artyom.arbatskiy@gmail.com>
2017-05-17 04:09:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendKitten = (kitten) => {
+  kittens.push(kitten)
+  return kittens
+}
+
+const destructivelyPrependKitten = (kitten) => {
+  kittens.unshift(kitten)
+  return kittens
+}
+
+const destructivelyRemoveLastKitten = () => {
+  kittens.pop()
+  return kittens
+}
+
+const destructivelyRemoveFirstKitten = () => {
+  kittens.shift()
+  return kittens
+}
+
+const appendKitten = (kitten) => {
+  let kittensEdited = kittens.slice(0)
+  kittensEdited.push(kitten)
+  return kittensEdited
+}
+
+const prependKitten = (kitten) => {
+  let kittensEdited = kittens.slice(0)
+  kittensEdited.unshift(kitten)
+  return kittensEdited
+}
+
+const removeLastKitten = () => {
+  let kittensEdited = kittens.slice(0)
+  return kittensEdited.splice(0, kittens.length - 1)
+}
+
+const removeFirstKitten = () => {
+  let kittensEdited = kittens.slice(0)
+  return kittensEdited.splice(1, kittens.length - 1)
+}
