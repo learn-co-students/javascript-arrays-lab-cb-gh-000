d1125bc69f7774dbc0287b52f22ae0b61144fbf5
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 18:14:26 -0400

    Create function destructivelyPrependKitten(name)

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const app = "I don't do much.";
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
