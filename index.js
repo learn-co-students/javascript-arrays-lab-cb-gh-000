a4c93a2dbc06eeeb4671fc6526606136f18fa530
Luke Gaynor <lukeg@iastate.edu>
2017-10-01 19:02:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+ }
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+function prependKitten(name) {
+  return [name, ...kittens]
+ }
+function removeLastKitten() {
+  var kittens = [...window.kittens]
+  kittens.length = kittens.length - 1
+  return kittens
+}
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
