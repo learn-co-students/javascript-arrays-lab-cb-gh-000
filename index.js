25ebd7a4c12aa1d0a06f6ca21bcc618b20fbf276
Victor Alagwu <victoralagwu@gmail.com>
2017-11-27 16:29:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+function appendKitten(name) {
+  var newArray = kittens.slice()
+  newArray.push(name);
+  return newArray;
+}
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1);
+}
+function prependKitten(name) {
+  var newArray =  kittens.slice();
+  newArray.unshift(name);
+  return newArray;
+}
