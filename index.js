3003ba318291439dc91917e97dfefa422ce0f269
Ahmad Taufiq Hidayat <ataufiq665@gmail.com>
2017-12-04 16:46:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  kittens.push(name)
+}
+
+function prependKitten(name){
+  kittens.unshift(name)
+}
+
+function removeLastKitten(){
+  kittens.pop()
+}
+
+function removeFirstKitten(){
+  kittens.shift()
+}
