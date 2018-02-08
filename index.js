85be37478fc054fa7e959fd55d764f401d7f2c79
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-08 16:48:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+//Adds a new name to the end of the kittens array, mutating the original kittens array
+function destructivelyAppendKitten(name){
+return kittens.push(name)
+}
+
+//Adds a new name to the start of the kittens array, mutating the original kittens array
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+//Removes the last name from the kittens array, mutating the original array
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop()
+}
+
+//Removes first name from the kittens array, mutating the original array
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift()
+}
+
+//Adds new name to the end of a newKittens array, leaving the original kittens array unchanged
+function appendKitten(name){
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+//Adds new name to the start of a newKittens array, leaving the original kittens array unchanged
+function prependKitten(name){
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
+//Removes all the names from the kittens array except for the last one, placing them into a new array which has the final name removed
+function removeLastKitten(name){
+  return kittens.slice(0, kittens.length-1)
+}
+
+//
+function removeFirstKitten(name){
+  return kittens.slice(1, kittens.length)
+}
