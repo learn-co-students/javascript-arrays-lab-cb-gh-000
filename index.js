3e93fde99ae0dd2f55bc7f12784e3f7fcb443212
Abdelrauof <34385173+abdelrauof97@users.noreply.github.com>
2018-04-22 15:01:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+const app = "I don't do much."
+kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens = kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens = kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens = kittens.pop(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens = kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens = kittens.unshift(name)
+}
+
+function appendKitten(name) {
+  return kittens.push(name)
+}
+
+function prependKitten(name) {
+  return kittens.unshift(name)
+}
+
+
+function removeLastKitten(name) {
+  return kittens.pop(name)
+}
+
+function removeFirstKitten(name) {
+  return kittens.unshift(name)
+}
