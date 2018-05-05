2ee779b21b53f5fb41f6105b3962a30096b2e977
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-05-05 07:13:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+}
+
+function destructivelyRemoveFirstKitten(kitten) {
+ kittens.shift(kitten);
+}
+
+function destructivelyRemoveLastKitten() {
+ kittens.pop();
+}
+
+function appendKitten(kitten) {
+  return [...kittens, kitten];
+}
+
+function prependKitten(kitten) {
+  return [kitten, ...kittens];
+}
+
+function removeLastKitten() {
+ return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
