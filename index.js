09647c7e192306a0632d4c1b30202f0c7af9874b
Nicholas Liang <ssss021418@gmail.com>
2018-05-09 15:01:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+var destructivelyAppendKitten=(name)=>{kittens.push(name);return kittens;}
+var destructivelyPrependKitten=(name)=>{kittens.unshift(name);return kittens;}
+var destructivelyRemoveLastKitten=()=>{kittens.pop();return kittens;}
+var destructivelyRemoveFirstKitten=()=>{kittens.shift();return kittens;}
+var appendKitten=(name)=>[...kittens, name]
+var prependKitten=(name)=>[name, ...kittens]
+var removeLastKitten=()=>kittens.slice(0,kittens.length-1);
+var removeFirstKitten=()=>kittens.slice(1);
