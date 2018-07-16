4a42551b784eb0d1961c0a8a1a9a0fb57434508e
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 12:53:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+kittens.shift()
+}
+
+function appendKitten(name) {
+  var kittens2 = kittens
+  return kittens2.concat(name)
+}
+
+function prependKitten(name){
+  var kittens2 = kittens
+  return [name,...kittens2]
+}
+
+function removeLastKitten(){
+  var kittens2 = kittens
+  return kittens2.slice(0,kittens2.length - 1)
+}
+
+function removeFirstKitten(){
+  var kittens2 = kittens
+  return kittens2.slice(1)
+}
