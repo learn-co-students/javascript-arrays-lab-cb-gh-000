24d62f012e19c9920c68b932af4add36299df24d
Jordan de Souza <ja3desou@uwaterloo.ca>
2019-06-06 19:01:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name)
 }
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.pop()
+}
