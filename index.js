7606aa69ed41115ffda1e5885e31d7cf54d6f5d1
Jordan de Souza <ja3desou@uwaterloo.ca>
2019-06-06 19:03:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function destructivelyRemoveLastKitten(){
 function destructivelyRemoveFirstKitten(){
   return kittens.shift()
 }
+
+function appendKitten(name){
+  var ne = kittens;
+  return ne.push(name)
+}
