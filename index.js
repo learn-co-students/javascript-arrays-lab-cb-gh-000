fb13190bc65044d006541df724c68a82aabd1a28
Jordan de Souza <ja3desou@uwaterloo.ca>
2019-06-06 19:00:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  return kittens.shift(name)
+  return kittens.push(name)
 }
